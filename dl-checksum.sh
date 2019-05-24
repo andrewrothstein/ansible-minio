5d5fed151f1dc38b0f7b56f12feddf35c5f77b06
Andrew Rothstein <andrew.rothstein@gmail.com>
2019-05-23 22:33:03 -0500

    up to server '2019-05-23T00-29-34Z' and client '2019-05-23T01-33-27Z'. improved the checksum downloader to make rolling versions easier #bash #ftw

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -11,17 +11,20 @@ dl()
     TS=$4
     FILE=$APPNAME.RELEASE.$TS.sha256sum
     URL=$MIRROR/$APPDOM/$APPNAME/release/$PLATFORM/$FILE
-    echo $URL
-    curl -L $URL
+    printf "  # `echo $URL`\n"
+    printf "  $PLATFORM: sha256:%s\n" `curl -sSL $URL | awk '{print $1}'`
 }
 
-SERVER_TS=2019-03-20T22-38-47Z
+
+SERVER_TS=2019-05-23T00-29-34Z
+printf "'$SERVER_TS':\n"
 for platform in darwin-amd64 linux-amd64 windows-amd64
 do
     dl server minio $platform $SERVER_TS
 done
 
-CLIENT_TS=2019-03-20T21-29-03Z
+CLIENT_TS=2019-05-23T01-33-27Z
+printf "'$CLIENT_TS':\n"
 for platform in darwin-amd64 linux-amd64 windows-amd64
 do
     dl client mc $platform $CLIENT_TS
