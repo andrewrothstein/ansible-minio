6b2b1eb29ced1b3801f78128a86e611f46947694
Andrew Rothstein <andrew.rothstein@gmail.com>
2019-07-03 13:10:55 -0500

    upgrade to latest

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -2,8 +2,8 @@
 
 MIRROR=https://dl.minio.io
 
-SERVER_TS=2019-06-19T18-24-42Z
-CLIENT_TS=2019-06-19T22-39-53Z
+SERVER_TS=2019-06-27T21-13-50Z
+CLIENT_TS=2019-06-27T21-31-45Z
 
 dl()
 {
@@ -13,18 +13,18 @@ dl()
     TS=$4
     FILE=$APPNAME.RELEASE.$TS.sha256sum
     URL=$MIRROR/$APPDOM/$APPNAME/release/$PLATFORM/$FILE
-    printf "  # `echo $URL`\n"
-    printf "  $PLATFORM: sha256:%s\n" `curl -sSL $URL | awk '{print $1}'`
+    printf "      # %s\n" $URL
+    printf "      %s: sha256:%s\n" $PLATFORM `curl -sSL $URL | awk '{print $1}'`
 }
 
 
-printf "'$SERVER_TS':\n"
+printf "    '$SERVER_TS':\n"
 for platform in darwin-amd64 linux-amd64 windows-amd64
 do
     dl server minio $platform $SERVER_TS
 done
 
-printf "'$CLIENT_TS':\n"
+printf "    '$CLIENT_TS':\n"
 for platform in darwin-amd64 linux-amd64 windows-amd64
 do
     dl client mc $platform $CLIENT_TS
